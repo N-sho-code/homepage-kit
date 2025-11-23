0dc297321124274cf7fb3d787799f5a8bb44bb78
N-sho-code <shou1408829@yahoo.co.jp>
2025-11-23 20:26:42 +0900

    一旦動く

---
+++ kit/hamburger-menu-template/js/script.js
@@ -0,0 +1,12 @@
+const burger = document.querySelector(".burger");
+const nav = document.querySelector(".nav-links");
+const navLinks = document.querySelectorAll(".nav-links li");
+
+burger.addEventListener("click",() => {
+    nav.classList.toggle("nav-active");
+
+    navLinks.forEach((link,index) => {
+        link.style.animation = `navLinksFade 0.3s ease forwards ${index/7+0.3}s`;
+    });
+    
+});
